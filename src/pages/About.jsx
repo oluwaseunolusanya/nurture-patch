457c87c6e1d7d3d9bbef5f2f387ed4f596e1b6a6
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2025-04-21 13:12:24 +0100

    Update About page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
   return (
-    <section>
+    <section className='text-green-600'>
       <h2 className="text-2xl font-bold mb-4">About Us</h2>
       <p>At Nurture Patch, we believe every child deserves a nurturing and inspiring environment. 
       We provide care for school-aged children with flexible after-school and holiday options.</p>
