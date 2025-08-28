2f92c9417b79bdd970a39512c68d94897d2abffa
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2025-08-28 18:44:46 +0100

    Removed unused modules.

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_PUBLIC_KEY } from '../config';
-import emailjs from '@emailjs/browser';
 
 function Booking() {
   const [formData, setFormData] = useState({
